34a57b6ddb881ace6f86bd5397726f8dedd04f2e
ahmedgamal93455@gmail.com <ahmedgamal93455@gmail.com>
2024-07-26 20:03:59 +0300

    Add project files.

---
+++ project 1 oop/project 1 oop.cpp
@@ -0,0 +1,44 @@
+
+// project 1 oop.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+class university {
+protected :
+    string studentName;
+    int id;
+    string subject;
+    string doctor;
+    int doctorId;
+public:
+    university() {
+        cout << "no data entered" << endl;
+    }
+   university(string k, int j) {
+        studentName = k;
+        id = j;
+        cout << "student is recorded" << endl;
+    }
+    university(string s, string d, int i) {
+        subject = s;
+        doctor = d;
+        doctorId = i;
+        cout << "subject and doctor recorded" << endl;
+    
+    }
+
+    
+};
+
+
+
+
+int main()
+{
+    
+    
+}
+
+
+
